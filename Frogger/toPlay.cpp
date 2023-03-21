aacc2623cd906d4e93f08bfde32b8df4f3342058
meloneng <meloneng@protonmail.com>
2023-03-21 14:51:52 -0300

    .

---
+++ Frogger/toPlay.cpp
@@ -0,0 +1,12 @@
+#ifdef __APPLE__
+    #define GL_SILENCE_DEPRECATION
+    #include <GLUT/glut.h>
+    #include <OpenGL/gl.h>
+    #include <OpenGL/glu.h>
+#else
+    #include <GL/glut.h>
+    #include <GL/gl.h>
+    #include <GL/glu.h>
+#endif
+
+
